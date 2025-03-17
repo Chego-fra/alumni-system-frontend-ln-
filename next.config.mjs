e561ce754a9ce70034847cb4bd331eaea0797076
Francis Chege <chegefrancis664@gmail.com>
2025-03-17 11:02:43 +0300

    added next.config.mjs chsnge the .eslintric.js linstyle to off

---
+++ next.config.mjs
@@ -0,0 +1,22 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: [
+      "images.pexels.com", 
+      "i.pinimg.com",
+      "encrypted-tbn0.gstatic.com",
+      "plus.unsplash.com",
+      "images.unsplash.com",
+      "randomuser.me",
+      "tse1.mm.bing.net",  
+      "tse2.mm.bing.net",
+      "tse4.mm.bing.net",
+      "tse3.mm.bing.net",
+      "tse5.mm.bing.net",
+      "tse6.mm.bing.net",
+      "www.pexels.com"
+    ],
+  },
+};
+
+export default nextConfig;
